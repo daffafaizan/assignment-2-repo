35c875ddc1a40f1a3d4ae09e7807424b65c08a95
daffafaizan <112247545+daffafaizan@users.noreply.github.com>
2022-09-14 20:34:49 +0700

    Initial commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_barang_katalog,
+        'nama': 'Daffa Muhammad Faizan',
+        'npm': 2106704156,
+    }
+    return render(request, 'katalog.html', context)
