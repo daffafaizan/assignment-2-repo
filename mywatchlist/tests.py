50ddde09725753f3544e7f24638038218f5695fd
daffafaizan <112247545+daffafaizan@users.noreply.github.com>
2022-09-21 22:31:25 +0700

    adding tests and modifying settings

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class Test_Cases(TestCase):
+    def test_xml_url(self):
+        self.client = Client()
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_json_url(self):
+        self.client = Client()
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_html_url(self):
+        self.client = Client()
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
