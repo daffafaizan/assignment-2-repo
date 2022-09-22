0bbfd929cbb3c0053f8d40e74da3326d81991e41
daffafaizan <112247545+daffafaizan@users.noreply.github.com>
2022-09-22 11:28:32 +0700

    updated models

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class MyWatchList(models.Model):
     watched = models.TextField()
     title = models.CharField(max_length=255)
-    rating = models.IntegerField()
+    rating = models.DecimalField(max_digits=5, decimal_places=1)
     release_date = models.TextField()
     review = models.TextField()
