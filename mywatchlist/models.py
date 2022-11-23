fce8c3f2e6ca129c3ec777d5be2f26edf4d5af83
daffafaizan <112247545+daffafaizan@users.noreply.github.com>
2022-11-23 20:45:50 +0700

    fixed models

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class MyWatchList(models.Model):
-    watched = models.TextField()
+    watched = models.BooleanField()
     title = models.CharField(max_length=255)
-    rating = models.DecimalField(max_digits=5, decimal_places=1)
+    rating = models.IntegerField()
     release_date = models.TextField()
     review = models.TextField()
